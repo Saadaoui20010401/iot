42378e2ba76959d4653f24b80339d5ae7030baa1
Saadaoui20010401 <noureddine.saadaoui.ensao@ump.ac.ma>
2025-12-02 21:17:12 +0100

    Update urls.py
     path("index/", views.dashboard, name="index"),

--- projet_iot/projet/projet/DHT/urls.py
+++ projet_iot/projet/projet/DHT/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("api",api.Dlist,name='json'),
     path("api/post",api.Dhtviews.as_view(),name='json'),
     #modification
+    
     path("", views.dashboard, name="dashboard"),
     path("latest/", views.latest_json, name="latest_json"),
 

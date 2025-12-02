74c4c096f2ea9ba0c5f479ff298a280b62b29309
Saadaoui20010401 <noureddine.saadaoui.ensao@ump.ac.ma>
2025-12-02 21:16:28 +0100

    Add comment for modification in urls.py

--- projet_iot/projet/projet/DHT/urls.py
+++ projet_iot/projet/projet/DHT/urls.py
@@ -5,6 +5,7 @@ from . import api
 urlpatterns = [
     path("api",api.Dlist,name='json'),
     path("api/post",api.Dhtviews.as_view(),name='json'),
+    #modification
     path("", views.dashboard, name="dashboard"),
     path("latest/", views.latest_json, name="latest_json"),
 
